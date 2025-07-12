ac0c6cf29c1601ad883f81af2c7d6a29aae39122
Mayko Rodrigues <mak213k@yahoo.com.br>
2025-07-12 15:02:28 -0300

    Add files via upload
    Arquivo inicial do projeto de automação.

---
+++ automacao_planilha.py
@@ -0,0 +1,37 @@
+
+"""
+Manipula arquivos xlsx
+"""
+import openpyxl
+
+"""
+Manipula arquivos csv
+"""
+import csv
+
+"""
+Cria cópia do dado
+"""
+from copy import copy
+
+"""
+Para manipulação de dados em diferentes formatos
+"""
+import pandas as pd
+
+
+"""
+Para leitura de arquivos excel antigo: extensão .xls
+"""
+import xlrd
+
+"""
+# Ler o arquivo Excel
+dataframe = pd.read_excel('caminho/do/arquivo.xlsx')
+
+# Exibir os dados
+print(dataframe
+
+"""
+
+
